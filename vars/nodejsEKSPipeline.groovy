95bd4eed7287a68a380c909356bc743cfac5614a
Ajay <ajaylearner01@gmail.com>
2025-08-26 00:12:08 +0530

    jenkins

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -77,7 +77,7 @@ def call(Map configMap){
                             script: """
                                 curl -s -H "Accept: application/vnd.github+json" \
                                     -H "Authorization: token ${GITHUB_TOKEN}" \
-                                    https://api.github.com/repos/daws-84s/catalogue/dependabot/alerts
+                                    https://api.github.com/repos/ajay630/catalogue/dependabot/alerts
                             """,
                             returnStdout: true
                         ).trim()
